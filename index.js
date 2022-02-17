a50a4cea8a311613113ff670eb035bfa9bc03f17
InsherahAL <juju.abulibdeh@gmail.com>
2022-02-17 15:25:40 +0200

    arrays

---
+++ index.js
@@ -0,0 +1,47 @@
+
+//     const arr1 = ['lily' ,'Insherah', 'haneen']
+//     const sayHello = function (Arr) {
+//         for (let i = 0; i < Arr.length; i++) {
+//             console.log('hello', Arr[i]);
+            
+//         }
+//     }
+// sayHello(arr1)
+// sayHello(['may'])
+
+// const formatArr = function (arr) {
+//     for (let i= 0; i < arr.length; i++) {
+//        arr[i] = 'hello ' + arr[i]
+    
+//     }
+// }
+// formatArr(arr1)
+// console.log(arr1);
+
+// const arr1 = ['lily' ,'Insherah', 'haneen']
+
+// const capitalize = function (arr) {
+ 
+//     for (let i = 0; i < arr.length; i++) {
+
+           
+//              arr[i] = arr[i].toUpperCase()
+        
+      
+       
+//     }
+// }
+// capitalize(arr1)
+// console.log(arr1);
+// const arr1 = ['lily',' Insherah', 'haneen']
+
+// const capitalize = function (arr) {
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i]=arr[i].split('')
+//   arr[i][0]=arr[i][0].toUpperCase()
+//   arr[i]= arr[i].join('')
+// }
+// }
+// capitalize(arr1)
+// console.log(arr1);
+
