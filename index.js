0c66f9493cde14e277c571b46c20734ab59aa47a
InsherahAL <juju.abulibdeh@gmail.com>
2022-02-23 20:07:24 +0200

    ggg

--- index.js
+++ index.js
@@ -91,147 +91,167 @@
 // user.signOut()
 // console.log(user);
 
-// const users = [{
-//     firstName: "Ahmad",
-//     lastName: 'kawasmi',
-//     userName: 'kirdahi',
-//     password: '276609',
-//     type: 'admin',
-//     age: 12,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// },
-// {
-//     firstName: "Yousef",
-//     lastName: 'Hamouri',
-//     type: 'teacher',
-//     userName: 'bigJoe',
-//     password: '276609',
-//     age: 13,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Ibrahim",
-//     lastName: 'Sharif',
-//     type: 'teacher',
-//     userName: 'Abra',
-//     password: '276609',
-//     age: 10,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Yazeed",
-//     lastName: 'Salameh',
-//     type: 'student',
-//     userName: 'yizzy',
-//     password: '276609',
-//     age: 8,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Insherah",
-//     lastName: 'Abu Libdeh',
-//     type: 'student',
-//     userName: 'inshallah',
-//     password: '276609',
-//     age: 8,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Hanin",
-//     lastName: 'Farhat',
-//     type: 'student',
-//     userName: 'hani2005',
-//     password: '276609',
-//     age: 8,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Khaled",
-//     lastName: 'Amro',
-//     type: 'student',
-//     userName: 'khokho',
-//     password: '276609',
-//     age: 7,
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// }, {
-//     firstName: "Bahaa",
-//     lastName: 'Jawabreh',
-//     type: 'student',
-//     age: 9,
-//     userName: 'bobo',
-//     password: '276609',
-//     isLoggedIn: false,
-//     signIn: function () {
-//         this.isLoggedIn = true
-//     },
-//     signOut: function () {
-//         this.isLoggedIn = false
-//     }
-// },
-// ]
-// console.log(users);
-// const findUser = (userName) => {
-//     for (let i = 0; i < users.length; i++) {
-//        if (users[i].userName == userName) {
-//            return  users[i]
-//        }
+const users = [{
+    firstName: "Ahmad",
+    lastName: 'kawasmi',
+    userName: 'kirdahi',
+    password: '276609',
+    type: 'admin',
+    age: 12,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+},
+{
+    firstName: "Yousef",
+    lastName: 'Hamouri',
+    type: 'teacher',
+    userName: 'bigJoe',
+    password: '276609',
+    age: 13,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Ibrahim",
+    lastName: 'Sharif',
+    type: 'teacher',
+    userName: 'Abra',
+    password: '276609',
+    age: 10,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Yazeed",
+    lastName: 'Salameh',
+    type: 'student',
+    userName: 'yizzy',
+    password: '276609',
+    age: 8,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Insherah",
+    lastName: 'Abu Libdeh',
+    type: 'student',
+    userName: 'inshallah',
+    password: '276609',
+    age: 8,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Hanin",
+    lastName: 'Farhat',
+    type: 'student',
+    userName: 'hani2005',
+    password: '276609',
+    age: 8,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Khaled",
+    lastName: 'Amro',
+    type: 'student',
+    userName: 'khokho',
+    password: '276609',
+    age: 7,
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+}, {
+    firstName: "Bahaa",
+    lastName: 'Jawabreh',
+    type: 'student',
+    age: 9,
+    userName: 'bobo',
+    password: '276609',
+    isLoggedIn: false,
+    signIn: function () {
+        this.isLoggedIn = true
+    },
+    signOut: function () {
+        this.isLoggedIn = false
+    }
+},
+]
+console.log(users);
+const findUser = (userName) => {
+    for (let i = 0; i < users.length; i++) {
+       if (users[i].userName == userName) {
+           return  users[i]
+       }
         
-//     }
-//     return {err:true,msg : ' cannot find user'}
-// }
-// const signInByUser = (userName,password)=>{
-//    const user = findUser(userName)
-//    if (user.err) {
-//        alert(user.msg)
-//        return
-//    }
-//    else if (user.password == password) {
-//        user.signIn()
-//        alert('welcome back Mr. '+user.userName)
+    }
+    return {err:true,msg : ' cannot find user'}
+}
+const signInByUser = (userName,password)=>{
+   const user = findUser(userName)
+   if (user.err) {
+       alert(user.msg)
+       return
+   }
+   else if (user.password == password) {
+       user.signIn()
+       alert('welcome back Mr. '+user.userName)
 
-//    }else{
-//        alert('wrong password!')
-//    }
+   }else{
+       alert('wrong password!')
+   }
   
 
-// }
+}
+
+const sayHello = (name) => alert('hello '+ name)
+const sayHi = (name) => alert('hi '+ name)
+const greet = function (greeting, personToGreet) {
+    greeting(personToGreet)
+}
+// greet(sayHi,'sameer')
+// greet((name)=> alert('bye '+ name),'sawsan')
+
+const sum = (num1,num2)=> num1 + num2
+const substract = (num1,num2)=> num1 + num2
+const multiply = (num1,num2)=> num1 + num2
+const divide = (num1,num2)=> num1 + num2
+
+const calculate = function (num1,num2,operation) {
+    alert(operation(num1,num2))
+}
+
+const user = users.find((u) => u.userName == 'khokho')
+alert(user.firstName)
 
 
 
